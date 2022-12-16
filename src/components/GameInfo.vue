955c702c90d179aaf3e985ade839426ef7831a64
ssshooter <ssshooterx@gmail.com>
2022-12-16 15:41:32 +0800

    CSS update

--- src/components/GameInfo.vue
+++ src/components/GameInfo.vue
@@ -69,6 +69,11 @@ defineProps<{
     .name {
       font-size: 20px;
       margin-bottom: 6px;
+      text-align: right;
+      width: 100%;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
     }
     .progress {
       margin-bottom: 6px;
